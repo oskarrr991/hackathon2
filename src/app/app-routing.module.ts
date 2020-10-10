e853f186d98dbe49c5c47e979f5e29fa7f10fb8d
MartinCepulionis <mcepulionis127@gmail.com>
2020-10-10 06:17:50 +0300

    style fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,9 @@ import {MainComponent} from "./pages/main/main.component";
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 
 const routes: Routes = [
-/*
-  {path: '**', redirectTo: '/main', pathMatch: 'full'},
-*/
-  {path: 'main', component: MainComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: '**', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'main', component: MainComponent}
 ];
 
 @NgModule({
